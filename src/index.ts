ec1ec21edc496e1910edda5ca7dd75c13d725ff6
Anni Wang <54481763+aninibread@users.noreply.github.com>
2024-08-16 11:19:55 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,6 @@
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello Nevi!');
+		return new Response('Hello Nevi!!!!');
 	},
 } satisfies ExportedHandler<Env>;
